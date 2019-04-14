a4c0a2d28854198607fcd0254413fe810d81b7da
Issa Rantisi <issa.rantisi@live.biu.ac.il>
2019-04-14 18:54:18 +0300

    added

---
+++ WpfApp1/ClientHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class ClientHandler : IClientHandler
+    {
+        public void HandleClient(TcpClient client)
+        {
+            new Task(() =>
+            {
+                using (NetworkStream stream = client.GetStream())
+                using (StreamReader reader = new StreamReader(stream))
+                using (StreamWriter writer = new StreamWriter(stream))
+                {
+                    string commandLine = reader.ReadLine();
+                    Console.WriteLine("Got command: {0}", commandLine);
+                    string result = ExecuteCommand(commandLine, client);
+                    writer.Write(result);
+                }
+                client.Close();
+            }).Start();
+        }
+
+        private string ExecuteCommand(string commandLine, TcpClient client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
